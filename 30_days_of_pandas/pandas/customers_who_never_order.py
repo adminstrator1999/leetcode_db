78841b51e427bd09e7cf9bb161a66e6f85e57e26
Bahromjon <hopebegmuhammadov@gmail.com>
2024-08-29 21:12:48 +0500

    "Customers Who Never Order" is finished

--- 30_days_of_pandas/pandas/customers_who_never_order.py
+++ 30_days_of_pandas/pandas/customers_who_never_order.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
     ids = customers.merge(orders, left_on='id', right_on='customerId', how='right')['id_x']
-    return customers[~customers['id'].isin(ids)][['name']].rename(columns={'name': 'Costumers'})
+    return customers[~customers['id'].isin(ids)][['name']].rename(columns={'name': 'Customers'})
 
 data = [[1, 'Joe'], [2, 'Henry'], [3, 'Sam'], [4, 'Max']]
 customers = pd.DataFrame(data, columns=['id', 'name']).astype({'id':'Int64', 'name':'object'})
