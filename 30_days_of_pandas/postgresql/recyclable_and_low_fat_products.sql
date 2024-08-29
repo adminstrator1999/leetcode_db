a055b3417fdb1e45f72a40cf40b1902e63e6130f
Bahromjon <hopebegmuhammadov@gmail.com>
2024-08-29 21:00:17 +0500

    "Recyclable and Low Fat Products" is finished

--- 30_days_of_pandas/postgresql/recyclable_and_low_fat_products.sql
+++ 30_days_of_pandas/postgresql/recyclable_and_low_fat_products.sql
@@ -5,3 +5,7 @@ insert into Products (product_id, low_fats, recyclable) values ('1', 'Y', 'Y')
 insert into Products (product_id, low_fats, recyclable) values ('2', 'N', 'Y')
 insert into Products (product_id, low_fats, recyclable) values ('3', 'Y', 'Y')
 insert into Products (product_id, low_fats, recyclable) values ('4', 'N', 'N')
+
+select product_id
+from products
+where low_fats = 'Y' and recyclable = 'Y'
